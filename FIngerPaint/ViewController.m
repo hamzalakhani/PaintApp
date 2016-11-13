4fdd14ed8716f1a5c8c3ba051ac370f20963e996
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-11-12 20:16:34 -0500

    painting

--- FIngerPaint/ViewController.m
+++ FIngerPaint/ViewController.m
@@ -7,22 +7,61 @@
 //
 
 #import "ViewController.h"
+#import "Drawing.h"
+#import "PaintPoint.h"
+#import "Lines.h"
+#import "Line.h"
 
 @interface ViewController ()
-
+// remove outlet
+@property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *panGesture;
+//@property (nonatomic) Lines *lines;
+@property (weak, nonatomic) IBOutlet Drawing *drawingView;
+@property (nonatomic) Line *line;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    
+    
+}
+- (IBAction)blueColor:(id)sender {
+    [self.view blueStroke:[sender Blue:self.view]];
+}
+- (IBAction)redColor:(id)sender {
+}
+- (IBAction)yellowColor:(id)sender {
+}
+- (IBAction)orangeColor:(id)sender {
+}
+- (IBAction)greenColor:(id)sender {
+}
+- (IBAction)blackColor:(id)sender {
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// when location in point
+
+- (IBAction)panLines:(UIPanGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        self.line = [Line new];
+        [self.drawingView.lines.line addObject:self.line];
+    }
+
+    if (sender.state == UIGestureRecognizerStateChanged) {
+
+        CGPoint point = [sender locationInView:self.drawingView];
+        PaintPoint *paintPoint = [[PaintPoint alloc]initWithPoint:point];
+        //paintpoints is the array in line class not the abpove variable init;
+        [self.line.paintPoints addObject:paintPoint];
+        [self.drawingView setNeedsDisplay];//causes it to call draw rect
+    }
+    
 }
 
 
